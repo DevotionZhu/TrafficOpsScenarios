ff7ffad9a061e1addc3ab023571f881679fc86d2
Mat Dolata <dolatamat@hotmail.com>
2017-08-25 14:41:41 -0700

    Initialize repository for traffic ops scenarios

---
+++ code/ImportTrafficOpsData.r
@@ -0,0 +1,10 @@
+#install.packages("devtools")
+#install.packages("readxl")
+#library(readxl)
+#library(devtools)
+Summary_of_Traffic_Operations_1_ <- read_excel("C:/Users/mjd/Downloads/Summary of Traffic Operations (1).xlsx")
+View(Summary_of_Traffic_Operations_1_)
+Ops1 <- Summary_of_Traffic_Operations_1_
+Ops1
+#dummytext
+hello
